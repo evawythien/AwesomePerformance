1b69daf92e68c0c7c4e08a4f26d1a2558aa5df89
Eva <evadvgarcia@gmail.com>
2021-10-25 21:13:22 +0200

    init

---
+++ AwesomePerformance.Console/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace AwesomePerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            HashSet<int> hashSetPool = new HashSet<int>(GenerateNumbers(100000, 1));
+            int[] sequence = GenerateNumbers(100000, 2);
+
+            sw.Start();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                bool contains = PoolManager.Contains(hashSetPool, sequence);
+            }
+
+            sw.Stop();
+
+            Console.WriteLine("HashSet pool milliseconds: " + sw.ElapsedMilliseconds);
+
+            int[] pool = GenerateNumbers(100000, 1);
+
+            sw.Start();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                bool contains = PoolManager.Contains(pool, sequence);
+            }
+
+            sw.Stop();
+
+            Console.WriteLine("Array pool milliseconds: " + sw.ElapsedMilliseconds);
+        }
+
+        public static int[] GenerateNumbers(int size, int? seed = null)
+        {
+            int[] number = new int[size];
+            Random random = seed != null ? new Random(seed.Value) : new Random();
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                number[i] = random.Next(0, int.MaxValue);
+            }
+
+            return number;
+        }
+    }
+}
---
+++ AwesomePerformance/PoolManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AwesomePerformance
+{
+    public class PoolManager
+    {
+        public static bool Contains(HashSet<int> pool, int[] sequence)
+        {
+            return sequence.All(d => pool.Contains(d));
+        }
+
+        public static bool Contains(int[] pool, int[] sequence)
+        {
+            return sequence.All(d => pool.Contains(d));
+        }
+    }
+}
